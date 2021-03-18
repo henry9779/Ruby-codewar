09352f81242951015995a7346ca728d71a39828e
Henry <vinsen1109@gmail.com>
2021-03-18 14:35:05 +0800

    merge_arrays

---
+++ merge_arrays.rb
@@ -0,0 +1,7 @@
+def merge_arrays(arr1, arr2)
+  (arr1 + arr2).sort.uniq
+end
+
+Test.assert_equals(merge_arrays([1, 2, 3, 4], [5, 6, 7, 8]), [1, 2, 3, 4, 5, 6, 7, 8])
+Test.assert_equals(merge_arrays([1, 3, 5, 7, 9], [10, 8, 6, 4, 2]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+Test.assert_equals(merge_arrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12]), [1, 2, 3, 4, 5, 7, 9, 10, 11, 12])
