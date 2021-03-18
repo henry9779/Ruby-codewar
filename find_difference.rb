1679e5f52af2e118ebb4a2ffcf68a2026af29388
Henry <vinsen1109@gmail.com>
2021-03-18 15:44:37 +0800

    sum_mix

--- find_difference.rb
+++ find_difference.rb
@@ -1,5 +1,6 @@
 def find_difference(a, b)
   (a.reduce(:*) - b.reduce(:*)).abs
+  # 將陣列內元素相成，A - B 然後用 abs 將值轉正
 end
 
 Test.assert_equals(find_difference([3, 2, 5], [1, 4, 4]), 14)
--- merge_arrays.rb
+++ merge_arrays.rb
@@ -1,5 +1,6 @@
 def merge_arrays(arr1, arr2)
   (arr1 + arr2).sort.uniq
+  # 將兩個陣列相加，再去掉重複的值
 end
 
 Test.assert_equals(merge_arrays([1, 2, 3, 4], [5, 6, 7, 8]), [1, 2, 3, 4, 5, 6, 7, 8])
