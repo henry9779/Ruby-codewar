482a591cf64f89fe8bc38bfdb1702051da217268
Henry <vinsen1109@gmail.com>
2021-03-19 14:01:18 +0800

    ball

---
+++ ball.rb
@@ -0,0 +1,27 @@
+class Ball
+  def initialize(type = "regular")
+    # new 的時候如果沒給值，就用 initialize 預設值
+    @type = type
+    # 將值設為區域變數
+  end
+  def ball_type
+    @type
+    # 回傳
+  end
+end
+
+# ========== 第二種寫法 =========
+class Ball
+  attr_reader :ball_type
+  # attr 取代下面三行
+  # def ball_type
+  #  @ball_type
+  # end
+
+  def initialize(ball_type = "regular")
+    @ball_type = ball_type
+  end
+end
+
+Test.assert_equals Ball.new.ball_type, "regular"
+Test.assert_equals Ball.new("super").ball_type, "super"
