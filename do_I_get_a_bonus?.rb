ad4cd765a0c8172adee67d3822e7ba1950e68838
Henry <vinsen1109@gmail.com>
2021-03-24 17:10:08 +0800

    bonus

---
+++ do_I_get_a_bonus?.rb
@@ -0,0 +1,13 @@
+def bonus_time(salary, bonus)
+  bonus == true ? "$"+(salary * 10).to_s : "$ #{salary}"
+end
+
+Test.describe("Basic tests") do
+  Test.assert_equals(bonus_time(10000, true), '$100000')
+  Test.assert_equals(bonus_time(25000, true), '$250000')
+  Test.assert_equals(bonus_time(10000, false), '$10000')
+  Test.assert_equals(bonus_time(60000, false), '$60000')
+  Test.assert_equals(bonus_time(2, true), '$20')
+  Test.assert_equals(bonus_time(78, false), '$78')
+  Test.assert_equals(bonus_time(67890, true), '$678900')
+end
